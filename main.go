97624ba881f7a5b36e0ef1ac897f130d47f4803b
Yerik K <yerik.koslowski@gmail.com>
2022-04-01 12:38:42 -0300

    Moved server Init back to main

--- main.go
+++ main.go
@@ -4,9 +4,31 @@ package main
 // Imports.
 import (
 	"TapTake-server/app"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
+
+var port int = 8090
 
 // Main Function.
 func main() {
-	app.Init()
+	// Get the Port.
+	EnvPort := os.Getenv("PORT")
+
+	if len(EnvPort) > 0 {
+		var err error
+		port, err = strconv.Atoi(EnvPort)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	log.Printf("Starting server on port %d", port)
+	err := http.ListenAndServe(":"+fmt.Sprint(port), app.Init())
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
